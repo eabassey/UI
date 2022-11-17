925267acabbdc370dc8923ceb0a6d82efe6be737
Enoch Abassey <enochabassey@gmail.com>
2022-11-17 17:37:34 +0000

    adding fonts and Logout component

---
+++ README.md
@@ -0,0 +1,2 @@
+# TODO WEB
+Foodstyle interview react todo test.
---
+++ src/components/Logout/Logout.tsx
@@ -0,0 +1,12 @@
+
+import './Logout.css';
+
+
+export const Logout = () => {
+
+    return (
+        <span className="Logout">
+            Logout
+        </span>
+    )
+}
