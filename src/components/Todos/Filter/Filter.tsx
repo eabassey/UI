1d83389b67b8a73dcb0d355753c8617d0e52ab63
Enoch Abassey <enochabassey@gmail.com>
2022-11-17 20:04:16 +0000

    adding filter area

---
+++ src/components/Todos/Filter/Filter.tsx
@@ -0,0 +1,22 @@
+
+import './Filter.css';
+
+export const Filter = () => {
+
+    return (
+        <>
+        <span className="Show">
+        Show:
+        </span>
+        <span className="All">
+        All
+        </span>
+        <span className="Completed">
+        Completed
+        </span>
+        <span className="Incompleted">
+        Incompleted
+        </span>
+        </>
+    );
+};
--- src/components/Todos/TodosContainer/TodosContainer.tsx
+++ src/components/Todos/TodosContainer/TodosContainer.tsx
@@ -1,4 +1,5 @@
 
+import { Filter } from '../Filter/Filter';
 import { Logo } from '../Logo/Logo';
 import { TextInput } from '../TextInput/TextInput';
 import { Title } from '../Title/Title';
@@ -9,13 +10,14 @@ import './TodosContainer.css';
 export const TodosContainer = () => {
 
     return (
-        <div className="Rectangle">
+        <div className="TodosContainer">
             <Logo />
             <Title />
             <TextInput />
             <TodoItem title="Make a todo list" />
             <TodoItem title="Make a todo list" />
             <TodoItem title="Make a todo list" />
+            <Filter />
         </div>
     );
 }
